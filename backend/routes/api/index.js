fb275d554fc504fbcf2d8c094a52f3825de819e1
Joubin T <jubin.taghdir@gmail.com>
2021-06-17 14:30:41 -0700

    complete authenticate phase

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,56 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// GET /api/set-token-cookie
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
+
+
+
+
+
+
+// //Test
+//1
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// // GET /api/restore-user
+//2
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// // GET /api/require-auth
+//3
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+//4
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
+              
 module.exports = router;
