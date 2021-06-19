e06e6051813ef0cf1dc37be06f3800fb390267c1
Joubin T <jubin.taghdir@gmail.com>
2021-06-18 21:36:05 -0700

    create component foundation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
+import Home from "./components/ProductsPage/Home";
+
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
