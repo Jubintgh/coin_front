67695233432505079c86ae28fd9a14a5e147a1e9
Joubin T <jubin.taghdir@gmail.com>
2021-06-27 19:55:17 -0700

    debug frontend

--- backend/db/seeders/20210620022434-demo-user.js
+++ backend/db/seeders/20210620022434-demo-user.js
@@ -9,16 +9,43 @@ module.exports = {
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password'),
+        profilePicUrl: './dummy-data/d-user/user1'
       },
       {
         email: faker.internet.email(),
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        profilePicUrl:'./dummy-data/d-user/user2'
       },
       {
         email: faker.internet.email(),
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        profilePicUrl:'./dummy-data/d-user/user3'
+      },
+      {
+        email: faker.internet.email(),
+        username: 'someguy',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        profilePicUrl:'./dummy-data/d-user/user4'
+      },
+      {
+        email: faker.internet.email(),
+        username: 'theguy',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        profilePicUrl: './dummy-data/d-user/user5'
+      },
+      {
+        email: faker.internet.email(),
+        username: 'anotherguy',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        profilePicUrl: './dummy-data/d-user/user6'
+      },
+      {
+        email: faker.internet.email(),
+        username: 'thisguy',
+        hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        profilePicUrl: './dummy-data/d-user/user7'
       },
     ], {});
   },
